63588556888aa1c85fc6467b4b6f18b9daebc261
Marlon Beck <marlon.beck99@hotmail.com>
2025-09-24 20:38:18 -0300

    Add project files.

---
+++ ControleAlugueisApp/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace ControleAlugueisApp.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
